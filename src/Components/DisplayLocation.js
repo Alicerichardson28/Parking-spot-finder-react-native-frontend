198ae52a35ae6824751328011ecf420d0df31f6f
github username <Paradafaii28@gmail.com>
2020-05-20 00:57:12 -0600

    add direction icon to displaypage

--- src/Components/DisplayLocation.js
+++ src/Components/DisplayLocation.js
@@ -1,54 +1,83 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Icon } from "react-native";
 import StarRating from 'react-native-star-rating';
-// import FontAwesome, { SolidIcons } from 'react-native-fontawesome';
- 
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faDirections } from '@fortawesome/free-solid-svg-icons'
+
 
 export default class DisplayLocation extends Component {
 
 render() {
    const displayText =this.props.data.map(data => 
-    <View style={styles.nameTitleContainer}>
-       <Text style={styles.nameTitle}>{data.name} </Text>
+    <View style={styles.mainContainer}>
+        <View style={styles.leftContainer}>
+            <Text style={styles.nameTitle}>{data.name}</Text>
             <StarRating 
                 rating ={data.rating}
                 containerStyle ={{width: '20%', marginLeft:15, marginTop: 3, marginBottom:3}}
                 starSize={10}
                 fullStarColor={'#f0a500'}
             />
-            <Text style={styles.address}>{data.vicinity}</Text>
+            <Text style={styles.address}>{data.vicinity} </Text>
+        </View>
+        <View style={styles.rightContainer}>
+            <FontAwesomeIcon 
+                style={styles.iconDirections} 
+                starSize={10}
+                icon={ faDirections } 
+            />
+        </View>
      </View>
     )
     return (
-            <View style={styles.title}>
-                <View>{displayText}</View>
-            </View>
+        <View style={styles.title}>
+            <View>{displayText}</View>
+        </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
     title: {
-        marginTop: 15,
+        marginTop: 10,
         height: 300,
         alignItems: 'center',
     },
     
-    nameTitleContainer: {
+    mainContainer: {
+        flexDirection: 'row',
         marginBottom: 15,
         borderBottomColor: '#dddddd',
         borderBottomWidth: 2,
         height: 65,
-        width: 400
+        width: 400,
     },
 
     nameTitle: {
-        marginLeft: 15
+        marginLeft: 15,
+        width: 300
     },
 
     address:{
         marginLeft: 15,
-        fontSize: 11
+        marginBottom: 10,
+        fontSize: 11,
+        width: 300,
+    },
+
+    iconDirections: {
+        marginRight: 40,
+        marginTop: 20,
+    },
+
+    leftContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems:'flex-start',
+        width: 300
+    },
+    rightContainer: {
     }
 })
 
