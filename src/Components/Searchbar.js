72e45549dfd7eb4544bce05e8ae5afac0e8753d4
Parada <paradafaii28@gmail.com>
2020-07-14 22:01:57 -0600

    fixed error searchbar file

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react'
-import { View } from 'react-native';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_API_KEY } from 'react-native-dotenv'
-import { Image, Text, View } from 'react-native';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 
 
 export default class Searchbar extends Component {
     render() {
         return (
-            <View>
                 <GooglePlacesAutocomplete
                     placeholder='Search'
                     minLength={2}
@@ -57,35 +53,9 @@ export default class Searchbar extends Component {
                             paddingBottom: 10,
                         },
                         predefinedPlacesDescription: {
-                            color: '#1faadb',   
-    render() {
-        return (
-            <View>
-                <GooglePlacesAutocomplete
-                    placeholder='Enter Location'
-                    minLength={2}
-                    autoFocus={false}
-                    returnKeyType={'default'}
-                    fetchDetails={true}
-                    styles={{
-                        textInputContainer: {
-                        backgroundColor: 'rgba(0,0,0,0)',
-                        borderTopWidth: 0,
-                        borderBottomWidth: 0,
-                        },
-                        textInput: {
-                        marginLeft: 0,
-                        marginRight: 0,
-                        height: 38,
-                        color: '#5d5d5d',
-                        fontSize: 16,
-                        },
-                        predefinedPlacesDescription: {
-                        color: '#1faadb',
-                        },
+                            color: '#1faadb',  
+                        }                    
                     }}
-                />
-            </View>
         )
     }
 }
